5b7f217bd9a438ad5caf919ea2461990644685ac
HamdiNur <hamdoonur26@gmail.com>
2025-01-31 11:53:33 +0300

    Add gradient background widget

---
+++ lib/widgets/gradient_background.dart
@@ -0,0 +1,25 @@
+// lib/widgets/gradient_background.dart
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const GradientBackground({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.green.shade50,
+            Colors.blue.shade50,
+          ],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
